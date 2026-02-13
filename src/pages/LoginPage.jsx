8dc066218928c4dc52cc0bca83a974ca247d08f6
Sco10969 <tommy2909@hotmail.fr>
2026-02-13 18:46:12 +0100

    ✨ feat: integrer la maquette Login (phase 1.2)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,55 @@
+import { Link } from 'react-router-dom'
+
 export default function LoginPage() {
-    return <main />
+    return (
+        <>
+            <nav className="main-nav">
+                <Link className="main-nav-logo" to="/">
+                    <img
+                        className="main-nav-logo-image"
+                        src="/assets/img/argentBankLogo.png"
+                        alt="Argent Bank Logo"
+                    />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </Link>
+                <div>
+                    <Link className="main-nav-item" to="/login">
+                        <i className="fa fa-user-circle"></i> Sign In
+                    </Link>
+                </div>
+            </nav>
+
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+
+                    <form>
+                        <div className="input-wrapper">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" />
+                        </div>
+
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" />
+                        </div>
+
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
+
+                        <button type="button" className="sign-in-button">
+                            Sign In
+                        </button>
+                    </form>
+                </section>
+            </main>
+
+            <footer className="footer">
+                <p className="footer-text">Copyright 2020 Argent Bank</p>
+            </footer>
+        </>
+    )
 }
