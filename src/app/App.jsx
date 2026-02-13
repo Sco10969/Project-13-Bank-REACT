17249a13e7cc32135a891709d5ab20c3d1bf705c
Sco10969 <tommy2909@hotmail.fr>
2026-02-13 16:52:28 +0100

    🚀 - Lancement Argent Bank Front
    - Initialisation du front avec Vite + React.
    - Mise en place des routes /, /login, /profile.
    - Setup Redux, ESLint et Prettier.
    - Integration des assets/CSS de la maquette dans le front.
    - Pages creees en structure (placeholders) pour preparer l'implementation fonctionnelle.

---
+++ src/app/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
+import ProfilePage from '../pages/ProfilePage'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
