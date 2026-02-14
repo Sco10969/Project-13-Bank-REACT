fab8a884594cc97d5e4fbeaa873afc7a2d952219
Sco10969 <tommy2909@hotmail.fr>
2026-02-14 10:53:51 +0100

    🏗️ chore: initialiser le store Redux avec auth (phase 1.4C)

--- src/app/store.js
+++ src/app/store.js
@@ -1,59 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-
-const state = {
-    value: null,
-    list: [],
-    owner: { firstName: '' },
-}
-
-const reducer = (currentState = state, action) => {
-    switch (action.type) {
-        case 'ADD_PRODUCT': {
-            const listWithNewProduct = [...currentState.list, action.payload]
-            return {
-                ...currentState,
-                list: listWithNewProduct,
-            }
-        }
-        case 'REMOVE_PRODUCT': {
-            const list = currentState.list.filter(
-                (item, index) => index !== action.payload
-            )
-            return {
-                ...currentState,
-                list,
-            }
-        }
-        case 'APPLY_VOUCHER': {
-            const withVoucherList = currentState.list.map((item) =>
-                item.title === 'Super Crémeux'
-                    ? {
-                          ...item,
-                          price: action.payload.price,
-                      }
-                    : item
-            )
-            return {
-                ...currentState,
-                list: withVoucherList,
-            }
-        }
-        case 'UPDATE_FIRSTNAME': {
-            const owner = {
-                ...currentState.owner,
-                firstName: action.payload,
-            }
-            return {
-                ...currentState,
-                owner,
-            }
-        }
-        default:
-            return currentState
-    }
-}
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
-    preloadedState: state,
-    reducer,
+    reducer: {
+        auth: authReducer
+    },
 })
