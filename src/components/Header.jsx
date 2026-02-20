9e0dd0b524464df56adc2d662cbde9b789faf6c4
Sco10969 <tommy2909@hotmail.fr>
2026-02-20 21:24:48 +0100

    ✨ feat: ajout de la gestion de la déconnexion dans le composant Header avec Redux (phase 1.5D)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../features/auth/authSlice'
+import { clearProfile } from '../features/profile/profileSlice'
 
 export default function Header() {
+    const dispatch = useDispatch()
+    const token = useSelector((state) => state.auth.token)
+    const firstName = useSelector((state) => state.profile.firstName)
+
+    const handleLogout = () => {
+        dispatch(logout())
+        dispatch(clearProfile())
+    }
+
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
@@ -12,9 +24,20 @@ export default function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link className="main-nav-item" to="/login">
-                    <i className="fa fa-user-circle"></i> Sign In
-                </Link>
+                {token ? (
+                    <>
+                        <Link className="main-nav-item" to="/profile">
+                            <i className="fa fa-user-circle"></i> {firstName || 'User'}
+                        </Link>
+                        <Link className="main-nav-item" to="/" onClick={handleLogout}>
+                            <i className="fa fa-sign-out"></i> Sign Out
+                        </Link>
+                    </>
+                ) : (
+                    <Link className="main-nav-item" to="/login">
+                        <i className="fa fa-user-circle"></i> Sign In
+                    </Link>
+                )}
             </div>
         </nav>
     )
